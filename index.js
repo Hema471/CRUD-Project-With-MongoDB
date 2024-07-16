a8edb7bd654c8420228966ed36a9952cc92dd903
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-07-16 13:27:24 +0300

    Implement global 404 error handler

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const welcomeRouter = require("./Routes/welcomeRoute");
 const courseRouter = require("./Routes/coursesRoutes");
+const text = require("./utils/StatusTexts");
 const app = express();
 app.use(express.json());
+
 app.use("/", welcomeRouter);
 app.use("/api/courses", courseRouter);
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    status: text.ERROR,
+    message: text.MESSAGE,
+  });
+});
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGO_URL;
 
 mongoose
   .connect(url)
